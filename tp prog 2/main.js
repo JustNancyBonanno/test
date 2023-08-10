cf908825f3fe0c248087828ed792f469e0655a90
nancy bonannon <nancy.bonanno.b@gmail.com>
2023-08-10 01:08:18 -0300

    se creo Función  MostrarDatos
    se agrego función para poder mandar los detos del login a la consola

---
+++ tp prog 2/main.js
@@ -0,0 +1,10 @@
+
+//document.getElementById("logear").addEventListener("click", function() {
+function mostrarDatos() {
+	var dato1 = document.getElementById("input1").value;
+	var dato2 = document.getElementById("input2").value;
+
+	console.log("Username:", dato1);
+	console.log("Password:", dato2);
+
+}
