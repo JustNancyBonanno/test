d99b04d7cfd1a1e42cc3d5e1dc1bae66d14382fb
nancy bonannon <nancy.bonanno.b@gmail.com>
2023-08-31 18:55:56 -0300

    archivos de TP 5

---
+++ tp-5/src/componentes/Formulario.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Boton from "./Boton";
+import Input from "./Input";
+
+export default class Formulario extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      materia: "",
+      docente: "",
+      horas: "",
+      codigo: ""
+    };
+  }
+  render() {
+    const { materia, docente, horas, codigo } = this.state;
+    return (
+      <div className="Formulario">
+        FORMULARIO DE INGRESO DE MATERIAS
+        <label>
+          {" "}
+          <Input titulo="Materia:" />{" "}
+        </label>
+        <label>
+          {" "}
+          <Input titulo="Docente:" />{" "}
+        </label>
+        <label>
+          {" "}
+          <Input titulo="Horas:" />{" "}
+        </label>
+        <label>
+          {" "}
+          <Input titulo="Codigo:" />{" "}
+        </label>
+      </div>
+    );
+  }
+}
